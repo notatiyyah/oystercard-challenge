c6ffd8a9a21c7f3c0e530844b97f544fbe688869
AtiyyahRawat <atiyyah.rawat@gmail.com>
2020-12-15 13:20:08 +0000

    Add balance attribute and ability to add to it

--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -1,4 +1,16 @@
 require "oyster_card"
 
 describe OysterCard do
+    let(:oyster) {OysterCard.new}
+    it { is_expected.to respond_to(:balance) }
+    it { is_expected.to respond_to(:top_up) }
+
+    it "sets the balance to 0 when instantiated" do
+        expect(oyster.balance).to eq 0
+    end
+
+    it "can be topped up by £5" do
+        oyster.top_up(5)
+        expect(oyster.balance).to eq 5
+    end
 end
